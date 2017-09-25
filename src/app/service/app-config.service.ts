66899bf40b5b1621893e035141c82ff0315c8ccb
TK11235 <TK11235@users.noreply.github.com>
2017-09-25 21:11:33 +0900

    fix: Peer接続履歴のソート順が逆だったのを修正

--- src/app/service/app-config.service.ts
+++ src/app/service/app-config.service.ts
@@ -50,8 +50,8 @@ export class AppConfigService {
 
     let history = await db.getPeerHistory();
     history.sort((a, b) => {
-      if (a.timestamp < b.timestamp) return -1;
-      if (a.timestamp > b.timestamp) return 1;
+      if (a.timestamp < b.timestamp) return 1;
+      if (a.timestamp > b.timestamp) return -1;
       return 0;
     });
 
