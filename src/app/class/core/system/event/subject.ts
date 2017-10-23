7c68ae7c513b33a15a65abb4e187c40ddb698a26
TK11235 <TK11235@users.noreply.github.com>
2017-10-23 22:03:30 +0900

    refactor: 引数名を妥当なものに

--- src/app/class/core/system/event/subject.ts
+++ src/app/class/core/system/event/subject.ts
@@ -2,11 +2,11 @@ import { Observer, Callback } from './observer';
 import { Event, EventContext } from './event';
 
 export interface Subject {
-  register(target: any): Observer;
-  unregister(target: any);
-  unregister(target: any, eventName: string);
-  unregister(target: any, callback: Callback<any>);
-  unregister(target: any, eventName: string, callback: Callback<any>);
+  register(key: any): Observer;
+  unregister(key: any);
+  unregister(key: any, eventName: string);
+  unregister(key: any, callback: Callback<any>);
+  unregister(key: any, eventName: string, callback: Callback<any>);
   registerListener(listener: Observer): Observer;
   unregisterListener(listener: Observer): Observer;
   call(eventName: string, data: any, sendTo?: string);
