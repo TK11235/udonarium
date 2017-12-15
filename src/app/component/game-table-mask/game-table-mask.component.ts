92d3bdfd3baffd2639ede09b46815315b1957331
TK11235 <TK11235@users.noreply.github.com>
2017-12-16 00:56:39 +0900

    refactor: GameTableMaskComponentの冗長なコードを整理 #11
    - 不要なコードの削除、変数とCSSの名称整理
    - 動作は現状維持

--- src/app/component/game-table-mask/game-table-mask.component.ts
+++ src/app/component/game-table-mask/game-table-mask.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, OnDestroy, NgZone, Input, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, OnDestroy, NgZone, Input, HostListener, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 import { GameCharacterSheetComponent } from '../game-character-sheet/game-character-sheet.component';
@@ -18,118 +18,79 @@ import { ImageFile } from '../../class/core/file-storage/image-file';
   styleUrls: ['./game-table-mask.component.css']
 })
 export class GameTableMaskComponent implements OnInit, OnDestroy, AfterViewInit {
-
-  @ViewChild('tableMask') gameChar: ElementRef;
-
   @Input() gameTableMask: GameTableMask = null;
-  //@Input() top: number = 0;
-  //@Input() left: number = 0;
   @Input() is3D: boolean = false;
 
   private dragAreaElement: HTMLElement = null;
 
-  private top: number = 0;
-  private left: number = 0;
+  get name(): string { return this.gameTableMask.name; }
+  get width(): number { return this.gameTableMask.width; }
+  get height(): number { return this.gameTableMask.height; }
+  get opacity(): number { return this.gameTableMask.opacity; }
+  get imageFile(): ImageFile { return this.gameTableMask.imageFile; }
+  get isLock(): boolean { return this.gameTableMask.isLock; }
+  set isLock(isLock: boolean) { this.gameTableMask.isLock = isLock; }
 
-  private offsetTop: number = 0;
-  private offsetLeft: number = 0;
-  private startTop: number = 0;
-  private startLeft: number = 0;
+  private _posX: number = 0;
+  private _posY: number = 0;
+  private _posZ: number = 0;
+
+  get posX(): number { return this._posX; }
+  set posX(posX: number) { this._posX = posX; this.setUpdateTimer(); }
+  get posY(): number { return this._posY; }
+  set posY(posY: number) { this._posY = posY; this.setUpdateTimer(); }
+  get posZ(): number { return this._posZ; }
+  set posZ(posZ: number) { this._posZ = posZ; this.setUpdateTimer(); }
+
   private delta: number = 1.0;
 
-  private callbackOnMouseDown: any = null;
-  private callbackOnMouseUp: any = null;
-  private callbackOnMouseMove: any = null;
-  private callbackOnPanelMouseDown: any = null;
+  private pointer: PointerCoordinate = { x: 0, y: 0, z: 0 };
+  private pointerOffset: PointerCoordinate = { x: 0, y: 0, z: 0 };
+  private pointerStart: PointerCoordinate = { x: 0, y: 0, z: 0 };
+  private pointerPrev: PointerCoordinate = { x: 0, y: 0, z: 0 };
 
-  private callbackOnDragstart: any = null;
+  private callbackOnMouseUp = (e) => this.onMouseUp(e);
+  private callbackOnMouseMove = (e) => this.onMouseMove(e);
 
   private updateIntervalFlag: boolean = true;
   private lastUpdateTimeStamp: number = 0;
   isDragging: boolean = false;
 
-  private prevTop: number = 0;
-  private prevLeft: number = 0;
-
-  private startDragPoint: PointerCoordinate = { x: 0, y: 0 };
-
   gridSize: number = 50;
-
-  private $gameCharElement: JQuery = null;
 
   private updateInterval: NodeJS.Timer = null;
   private allowOpenContextMenu: boolean = false;
 
   constructor(
-    private ngZone: NgZone,
-    //private gameRoomService: GameRoomService,
     private contextMenuService: ContextMenuService,
-    //private modalService: ModalService,
     private panelService: PanelService,
     private elementRef: ElementRef,
-    private changeDetector: ChangeDetectorRef,
     private pointerDeviceService: PointerDeviceService
   ) { }
 
   ngOnInit() {
+    this.setPosition(this.gameTableMask);
     EventSystem.register(this)
       .on('UPDATE_GAME_OBJECT', -1000, event => {
         if (event.isSendFromSelf || event.data.identifier !== this.gameTableMask.identifier) return;
-        //console.log('UPDATE_GAME_OBJECT GameCharacterComponent ' + this.gameCharacter.identifier);
-        //if (event.sender === NetworkProxy.myPeerId) return;
         this.isDragging = false;
-
-        this.setPosition(this.gameTableMask.location.x, this.gameTableMask.location.y);
-        //if (event.data.identifier === this.gameCharacter.identifier) this.changeDetector.markForCheck();
+        this.setPosition(this.gameTableMask);
       });
   }
 
   ngAfterViewInit() {
     console.log('ngAfterViewInit');
     this.dragAreaElement = this.findDragAreaElement(this.elementRef.nativeElement);
-
-    let element = this.elementRef.nativeElement;
-
-    this.$gameCharElement = $(this.gameChar.nativeElement);
-
-    this.setPosition(this.gameTableMask.location.x, this.gameTableMask.location.y);
-
-    this.callbackOnMouseDown = (e) => this.onMouseDown(e);
-    this.callbackOnMouseUp = (e) => this.onMouseUp(e);
-    this.callbackOnMouseMove = (e) => this.onMouseMove(e);
-    this.callbackOnPanelMouseDown = (e) => this.onPanelMouseDown(e);
-    this.callbackOnDragstart = (e) => this.onDragstart(e);
-
-    element.addEventListener('mousedown', this.callbackOnMouseDown, false);
-    //this.gameCharImage.nativeElement.addEventListener('mousedown', this.callbackOnMouseDown, false);
-    //this.gamePanel.nativeElement.addEventListener('mousedown', this.callbackOnPanelMouseDown, false);
-    element.addEventListener('dragstart', this.callbackOnDragstart, false);
   }
 
   ngOnDestroy() {
     EventSystem.unregister(this);
-    let element = this.elementRef.nativeElement;
-
-    element.removeEventListener('mousedown', this.callbackOnMouseDown, false);
-    //this.gameCharImage.nativeElement.removeEventListener('mousedown', this.callbackOnMouseDown, false);
-
-    document.body.removeEventListener('mouseup', this.callbackOnMouseUp, false);
-    document.body.removeEventListener('mousemove', this.callbackOnMouseMove, false);
-
-    //this.gamePanel.nativeElement.removeEventListener('mousedown', this.callbackOnPanelMouseDown, false);
-    element.removeEventListener('dragstart', this.callbackOnDragstart, false);
-
-    this.callbackOnMouseDown = null;
-    this.callbackOnMouseUp = null;
-    this.callbackOnMouseMove = null;
-    this.callbackOnPanelMouseDown = null;
-    this.callbackOnDragstart = null;
+    this.removeMouseEventListeners();
   }
 
-  private calcLocalCoordinate() {
-    let coordinate = PointerDeviceService.convertToLocal(this.pointerDeviceService.pointers[0], this.dragAreaElement);
-    this.top = coordinate.y;
-    this.left = coordinate.x;
+  private calcLocalCoordinate(pointer: PointerCoordinate): PointerCoordinate {
+    let coordinate = PointerDeviceService.convertToLocal(pointer, this.dragAreaElement);
+    return { x: coordinate.x, y: coordinate.y, z: coordinate.z };
   }
 
   private findDragAreaElement(parent: HTMLElement): HTMLElement {
@@ -141,138 +102,90 @@ export class GameTableMaskComponent implements OnInit, OnDestroy, AfterViewInit 
     return null;
   }
 
+  @HostListener('dragstart', ['$event'])
   onDragstart(e) {
     console.log('Dragstart Cancel !!!!');
     e.stopPropagation();
     e.preventDefault();
   }
 
-  onPanelMouseDown(e: MouseEvent) {
-    e.stopPropagation();
-    this.isDragging = false;
-  }
-
+  @HostListener('mousedown', ['$event'])
   onMouseDown(e: any) {
     console.log('GameCharacterComponent mousedown !!!');
-
-    //if (this.gameTableMask.isLock) return;
-
     this.allowOpenContextMenu = true;
-    this.calcLocalCoordinate();
-
+    this.pointer = this.calcLocalCoordinate(this.pointerDeviceService.pointers[0]);
 
     this.isDragging = true;
 
-    this.offsetTop = this.gameTableMask.location.y - this.top;
-    this.offsetLeft = this.gameTableMask.location.x - this.left;
+    this.pointerOffset.y = this.posY - this.pointer.y;
+    this.pointerOffset.x = this.posX - this.pointer.x;
 
     this.delta = 1.0;
-    this.startTop = this.top;
-    this.startLeft = this.left;
+    this.pointerStart.y = this.pointerPrev.y = this.pointer.y;
+    this.pointerStart.x = this.pointerPrev.x = this.pointer.x;
 
-    this.prevTop = this.top;
-    this.prevLeft = this.left;
-
-    document.body.addEventListener('mouseup', this.callbackOnMouseUp, false);
-    document.body.addEventListener('mousemove', this.callbackOnMouseMove, false);
+    this.addMouseEventListeners();
 
     console.log('onSelectedGameCharacter', this.gameTableMask.identifier);
     EventSystem.trigger('SELECT_TABLETOP_OBJECT', { identifier: this.gameTableMask.identifier, className: 'GameCharacter' });
 
     e.preventDefault();
 
-    this.startDragPoint = this.pointerDeviceService.pointers[0];
-
     // TODO:もっと良い方法考える
-    if (this.gameTableMask.isLock) {
+    if (this.isLock) {
       EventSystem.trigger('DRAG_LOCKED_OBJECT', {});
     }
   }
 
   onMouseUp(e: any) {
-    //console.log('GameCharacterComponent mouseup !!!!');
     setTimeout(() => { this.allowOpenContextMenu = false; }, 0);
     this.isDragging = false;
 
-    document.body.removeEventListener('mouseup', this.callbackOnMouseUp, false);
-    document.body.removeEventListener('mousemove', this.callbackOnMouseMove, false);
+    this.removeMouseEventListeners();
 
-    let deltaX = this.gameTableMask.location.x % 25;
-    let deltaY = this.gameTableMask.location.y % 25;
+    let deltaX = this.posX % 25;
+    let deltaY = this.posY % 25;
 
-    this.gameTableMask.location.x += deltaX < 12.5 ? -deltaX : 25 - deltaX;
-    this.gameTableMask.location.y += deltaY < 12.5 ? -deltaY : 25 - deltaY;
-    this.setPosition(this.gameTableMask.location.x, this.gameTableMask.location.y);
-
-    if (this.updateInterval === null) {
-      this.updateInterval = setTimeout(() => {
-        this.gameTableMask.update();
-        this.updateInterval = null;
-      }, 66);
-    }
+    this.posX += deltaX < 12.5 ? -deltaX : 25 - deltaX;
+    this.posY += deltaY < 12.5 ? -deltaY : 25 - deltaY;
 
     e.preventDefault();
   }
 
   onMouseMove(e: any) {
-    if (this.startDragPoint.x !== this.pointerDeviceService.pointers[0].x || this.startDragPoint.y !== this.pointerDeviceService.pointers[0].y) {
+    if (this.isDragging && !this.isLock) {
+      this.pointer = this.calcLocalCoordinate(this.pointerDeviceService.pointers[0]);
+      if ((this.pointerPrev.y === this.pointer.y && this.pointerPrev.x === this.pointer.x)) return;
       this.allowOpenContextMenu = false;
-    }
-    if (this.isDragging) {
-      if (this.gameTableMask.isLock) return;
-      this.calcLocalCoordinate();
-      if ((this.prevTop === this.top && this.prevLeft === this.left)) return;
 
-      let width: number = this.gridSize * this.gameTableMask.width;
-      let height: number = this.gridSize * this.gameTableMask.height;
+      let width: number = this.gridSize * this.width;
+      let height: number = this.gridSize * this.height;
 
-      //this.gameTableMask.location.x = this.left + (this.offsetLeft * this.delta) + (-(width / 2) * (1.0 - this.delta));
-      //this.gameTableMask.location.y = this.top + (this.offsetTop * this.delta) + (-(height / 2) * (1.0 - this.delta));
+      this.posX = this.pointer.x + (this.pointerOffset.x * this.delta) + (-(width / 2) * (1.0 - this.delta));
+      this.posY = this.pointer.y + (this.pointerOffset.y * this.delta) + (-(height / 2) * (1.0 - this.delta));
 
-      this.gameTableMask.location.x = this.left + (this.offsetLeft * this.delta) + (-(width / 2) * (1.0 - this.delta));
-      this.gameTableMask.location.y = this.top + (this.offsetTop * this.delta) + (-(height / 2) * (1.0 - this.delta));
-
-      this.setPosition(this.gameTableMask.location.x, this.gameTableMask.location.y);
-
-      if (this.updateInterval === null) {
-        this.updateInterval = setTimeout(() => {
-          this.gameTableMask.update();
-          this.updateInterval = null;
-        }, 66);
-      }
-
-      let distanceY = this.startTop - this.top;
-      let distanceX = this.startLeft - this.left;
-
-      let distance = 9999;//(size * 4) / (Math.sqrt(distanceY * distanceY + distanceX * distanceX) + (size * 4));
-
-      if (distance < this.delta) {
-        this.delta = distance;
-      }
-      this.prevTop = this.top;
-      this.prevLeft = this.left;
+      this.pointerPrev.y = this.pointer.y;
+      this.pointerPrev.x = this.pointer.x;
     }
   }
 
+  @HostListener('contextmenu', ['$event'])
   onContextMenu(e: Event) {
     console.log('onContextMenu');
     e.stopPropagation();
     e.preventDefault();
+
+    this.isDragging = false;
+    this.removeMouseEventListeners();
+
     if (this.allowOpenContextMenu === false) return;
     let potison = this.pointerDeviceService.pointers[0];
     console.log('mouseCursor', potison);
     this.contextMenuService.open(potison, [
-      (this.gameTableMask.isLock ? {
-        name: '固定解除', action: () => {
-          this.gameTableMask.isLock = false;
-          this.gameTableMask.update();
-        }
-      } : {
-          name: '固定する', action: () => {
-            this.gameTableMask.isLock = true;
-            this.gameTableMask.update();
-          }
-        }),
+      (this.isLock
+        ? { name: '固定解除', action: () => { this.isLock = false; } }
+        : { name: '固定する', action: () => { this.isLock = true; } }
+      ),
       { name: 'マップマスクを編集', action: () => { this.showDetail(this.gameTableMask); } },
       {
         name: 'コピーを作る', action: () => {
@@ -284,21 +197,40 @@ export class GameTableMaskComponent implements OnInit, OnDestroy, AfterViewInit 
         }
       },
       { name: '削除する', action: () => { this.gameTableMask.destroy(); } },
-    ], this.gameTableMask.name);
+    ], this.name);
+  }
+
+  private setUpdateTimer() {
+    if (this.updateInterval === null) {
+      this.updateInterval = setTimeout(() => {
+        this.gameTableMask.location.x = this.posX;
+        this.gameTableMask.location.y = this.posY;
+        this.gameTableMask.posZ = this.posZ;
+        this.updateInterval = null;
+      }, 66);
+    }
   }
 
   private showDetail(gameObject: GameTableMask) {
-    console.log('onSelectedGameObject <' + gameObject.aliasName + '>', gameObject.identifier);
-    EventSystem.trigger('SELECT_TABLETOP_OBJECT', { identifier: gameObject.identifier, className: gameObject.aliasName });
-    //this.modalService.open(GameCharacterSheetComponent);
     let coordinate = this.pointerDeviceService.pointers[0];
     let option: PanelOption = { left: coordinate.x - 200, top: coordinate.y - 150, width: 400, height: 300 };
     let component = this.panelService.open<GameCharacterSheetComponent>(GameCharacterSheetComponent, option);
     component.tabletopObject = gameObject;
   }
 
-  setPosition(x: number, y: number) {
-    if (this.$gameCharElement) this.$gameCharElement.css('transform', 'translateX(' + x + 'px) translateY(' + y + 'px)');
-    //if (this.$gameCharElement) this.$gameCharElement.css('transform', 'translateX(' + x + 'px) translateY(' + y + 'px)');
+  private addMouseEventListeners() {
+    document.body.addEventListener('mouseup', this.callbackOnMouseUp, false);
+    document.body.addEventListener('mousemove', this.callbackOnMouseMove, false);
+  }
+
+  private removeMouseEventListeners() {
+    document.body.removeEventListener('mouseup', this.callbackOnMouseUp, false);
+    document.body.removeEventListener('mousemove', this.callbackOnMouseMove, false);
+  }
+
+  private setPosition(object: GameTableMask) {
+    this._posX = object.location.x;
+    this._posY = object.location.y;
+    this._posZ = object.posZ;
   }
 }
