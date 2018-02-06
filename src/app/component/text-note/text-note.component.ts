541c8aa9f59e75f95539e87fc2b1e1f28709e722
TK11235 <TK11235@users.noreply.github.com>
2018-02-06 23:02:20 +0900

    perf: 共有メモの回転角度を細かく設定できるように変更
    8方向→24方向

--- src/app/component/text-note/text-note.component.ts
+++ src/app/component/text-note/text-note.component.ts
@@ -209,8 +209,8 @@ export class TextNoteComponent implements OnInit {
     e.stopPropagation();
     this.removeRotateEventListeners();
 
-    this.rotate = this.rotate < 0 ? this.rotate - 22.5 : this.rotate + 22.5;
-    this.rotate -= (this.rotate) % 45;
+    this.rotate = this.rotate < 0 ? this.rotate - 7.5 : this.rotate + 7.5;
+    this.rotate -= (this.rotate) % 15;
   }
 
   @HostListener('contextmenu', ['$event'])
