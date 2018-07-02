893c4e7ded63cde0dc7bf9e02a5f05e9840c8622
TK11235 <TK11235@users.noreply.github.com>
2018-07-02 21:24:22 +0900

    style: コード整形

--- src/app/component/file-storage/file-storage.component.ts
+++ src/app/component/file-storage/file-storage.component.ts
@@ -1,23 +1,21 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
-import { PanelService } from '../../service/panel.service';
-import { ModalService } from '../../service/modal.service';
+import { FileArchiver } from '../../class/core/file-storage/file-archiver';
 import { FileStorage } from '../../class/core/file-storage/file-storage';
 import { ImageFile } from '../../class/core/file-storage/image-file';
-import { Network, EventSystem } from '../../class/core/system/system';
-import { FileArchiver } from '../../class/core/file-storage/file-archiver';
+import { EventSystem, Network } from '../../class/core/system/system';
+import { PanelService } from '../../service/panel.service';
 
 @Component({
   selector: 'file-storage',
   templateUrl: './file-storage.component.html',
   styleUrls: ['./file-storage.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FileStorageComponent implements OnInit, OnDestroy, AfterViewInit {
 
   fileStorageService = FileStorage.instance;
   constructor(
-    //private fileStorageService: FileStorageService,
     private changeDetector: ChangeDetectorRef,
     private panelService: PanelService
   ) { }
@@ -48,5 +46,4 @@ export class FileStorageComponent implements OnInit, OnDestroy, AfterViewInit {
     console.log('onSelectedFile', file);
     EventSystem.call('SELECT_FILE', { fileIdentifier: file.identifier }, Network.peerId);
   }
-
 }
