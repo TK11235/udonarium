b0337b2eea1dd7d6e32df361fe927b0c80da5eee
TK11235 <TK11235@users.noreply.github.com>
2018-08-22 00:14:11 +0900

    fix: e2eテストのコードをangular-cliのデフォルト設定に合わせる
    現在未使用

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { UnrealDiceOnlinePage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('unreal-dice-online App', () => {
-  let page: UnrealDiceOnlinePage;
+describe('workspace-project App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new UnrealDiceOnlinePage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to udonarium!');
   });
 });
