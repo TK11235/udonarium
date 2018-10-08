22b7d2866f7b021de336e14bfdfe72a8b150efa1
TK11235 <TK11235@users.noreply.github.com>
2018-10-08 21:44:28 +0900

    fix: 冗長な処理を整理

--- src/app/class/core/file-storage/image-sharing-system.ts
+++ src/app/class/core/file-storage/image-sharing-system.ts
@@ -1,9 +1,10 @@
-import { ArrayBuffer } from '@angular/http/src/static_request';
-import { EventSystem, Event, Network } from '../system/system';
-import { ImageStorage, Catalog } from './image-storage';
-import { ImageFile, ImageContext, ImageState } from './image-file';
-import { MimeType } from './mime-type';
+import { UUID } from '../synchronize-object/uuid';
+import { EventSystem, Network } from '../system/system';
+import { BufferSharingTask } from './buffer-sharing-task';
 import { FileReaderUtil } from './file-reader-util';
+import { ImageContext, ImageFile, ImageState } from './image-file';
+import { Catalog, ImageStorage } from './image-storage';
+import { MimeType } from './mime-type';
 
 export class FileSharingSystem {
   private static _instance: FileSharingSystem
@@ -12,10 +13,8 @@ export class FileSharingSystem {
     return FileSharingSystem._instance;
   }
 
-  private transmissionTimers: { [fileIdentifier: string]: NodeJS.Timer } = {};
+  private taskMap: Map<string, BufferSharingTask<ImageContext[]>> = new Map();
   private maxTransmission: number = 1;
-
-  private lazyTimer: NodeJS.Timer;
 
   private constructor() {
     console.log('FileSharingSystem ready...');
@@ -62,7 +61,7 @@ export class FileSharingSystem {
         if (event.isSendFromSelf) return;
 
         let request: Catalog = event.data.identifiers;
-        let randomRequest: { identifier: string, state: number }[] = [];
+        let randomRequest: Catalog = [];
 
         for (let item of request) {
           let image: ImageFile = ImageStorage.instance.get(item.identifier);
@@ -72,63 +71,9 @@ export class FileSharingSystem {
 
         if (this.isTransmission() === false && 0 < randomRequest.length) {
           // 送信
-          console.log('REQUEST_FILE_RESOURE ImageStorageService Send!!! ' + event.data.receiver + ' -> ' + randomRequest);
-
-          let updateImages: ImageContext[] = [];
-          let byteSize: number = 0;
-          let maxSize = 1024 * 1024 * 0.5;
-
-          // Fisher-Yates
-          for (let i = randomRequest.length - 1; 0 <= i; i--) {
-            let rand = Math.floor(Math.random() * (i + 1));
-            [randomRequest[i], randomRequest[rand]] = [randomRequest[rand], randomRequest[i]];
-          }
-
-          randomRequest.sort((a, b) => {
-            if (a.state < b.state) return -1;
-            if (a.state > b.state) return 1;
-            return 0;
-          });
-
-          for (let i = 0; i < randomRequest.length; i++) {
-            let item: { identifier: string, state: number } = randomRequest[i];
-            let image: ImageFile = ImageStorage.instance.get(item.identifier);
-
-            let context: ImageContext = { identifier: image.identifier, name: image.name, type: '', blob: null, url: null, thumbnail: { type: '', blob: null, url: null, } };
-
-            if (image.state === ImageState.URL) {
-              context.url = image.url;
-            } else if (item.state === ImageState.NULL) {
-              context.thumbnail.blob = image.thumbnail.blob;//
-              context.thumbnail.type = image.thumbnail.type;
-            } else {
-              context.blob = image.blob;//
-              context.type = image.blob.type;
-            }
-
-            if (0 < byteSize && context.blob && maxSize < byteSize + context.blob.size) break;
-
-            console.log(item);
-            console.log(context);
-
-            updateImages.push(context);
-            byteSize += context.blob ? context.blob.size : 100;
-          }
-
-          this.startTransmission(updateImages[0].identifier, event.sendFrom);
-          EventSystem.call('START_FILE_TRANSMISSION', { fileIdentifier: updateImages[0].identifier }, event.data.receiver);
-
-          /* hotfix issue #1 */
-          for (let context of updateImages) {
-            if (context.thumbnail.blob) {
-              context.thumbnail.blob = <any>await FileReaderUtil.readAsArrayBufferAsync(context.thumbnail.blob);
-            } else if (context.blob) {
-              context.blob = <any>await FileReaderUtil.readAsArrayBufferAsync(context.blob);
-            }
-          }
-          /* */
-
-          EventSystem.call('UPDATE_FILE_RESOURE', updateImages, event.data.receiver);
+          let updateImages: ImageContext[] = this.makeSendUpdateImages(randomRequest);
+          console.log('REQUEST_FILE_RESOURE ImageStorageService Send!!! ' + event.data.receiver + ' -> ' + updateImages.length);
+          this.startSendTransmission(updateImages, event.data.receiver);
         } else {
           // 中継
           let candidatePeers: string[] = event.data.candidatePeers;
@@ -138,68 +83,83 @@ export class FileSharingSystem {
           for (let peer of candidatePeers) {
             console.log('REQUEST_FILE_RESOURE ImageStorageService Relay!!! ' + peer + ' -> ' + event.data.identifiers);
             EventSystem.call(event, peer);
-            break;
+            return;
           }
+          console.log('REQUEST_FILE_RESOURE ImageStorageService あぶれた...' + event.data.receiver, randomRequest.length, this.taskMap);
         }
       })
       .on('UPDATE_FILE_RESOURE', event => {
-        let updateImages: ImageContext[] = event.data;
+        let updateImages: ImageContext[] = event.data.updateImages;
         console.log('UPDATE_FILE_RESOURE ImageStorageService ' + event.sendFrom + ' -> ', updateImages);
         for (let context of updateImages) {
           if (context.blob) context.blob = new Blob([context.blob], { type: context.type });
           if (context.thumbnail.blob) context.thumbnail.blob = new Blob([context.thumbnail.blob], { type: context.thumbnail.type });
           ImageStorage.instance.add(context);
         }
-        this.stopTransmission(updateImages[0].identifier);
-        ImageStorage.instance.synchronize();
-        EventSystem.call('COMPLETE_FILE_TRANSMISSION', { fileIdentifier: updateImages[0].identifier }, event.sendFrom);
       })
       .on('START_FILE_TRANSMISSION', event => {
-        console.log('START_FILE_TRANSMISSION ' + event.data.fileIdentifier);
-        this.startTransmission(event.data.fileIdentifier, event.sendFrom);
-      })
-      .on('COMPLETE_FILE_TRANSMISSION', event => {
-        console.log('COMPLETE_FILE_TRANSMISSION ' + event.data.fileIdentifier);
-        this.stopTransmission(event.data.fileIdentifier);
-        if (!event.isSendFromSelf) ImageStorage.instance.synchronize();
-      })
-      .on('TIMEOUT_FILE_TRANSMISSION', event => {
-        console.log('TIMEOUT_FILE_TRANSMISSION ' + event.data.fileIdentifier);
-        this.stopTransmission(event.data.fileIdentifier);
-      })
+        let identifier = event.data.taskIdentifier
+        console.log('START_FILE_TRANSMISSION ' + identifier);
+        this.startReceiveTransmission(identifier);
+      });
   }
 
   private destroy() {
     EventSystem.unregister(this);
   }
 
-  private startTransmission(identifier: string, sendFrom: string) {
+  private async startSendTransmission(updateImages: ImageContext[], sendTo: string) {
+    let identifier = UUID.generateUuid();
+    this.taskMap.set(identifier, null);
+    EventSystem.call('START_FILE_TRANSMISSION', { taskIdentifier: identifier }, sendTo);
+
+    /* hotfix issue #1 */
+    for (let context of updateImages) {
+      if (context.thumbnail.blob) {
+        context.thumbnail.blob = <any>await FileReaderUtil.readAsArrayBufferAsync(context.thumbnail.blob);
+      } else if (context.blob) {
+        context.blob = <any>await FileReaderUtil.readAsArrayBufferAsync(context.blob);
+      }
+    }
+    /* */
+
+    let task = await BufferSharingTask.createSendTask(updateImages, sendTo, identifier);
+    this.taskMap.set(task.identifier, task);
+
+    task.onfinish = (task, data) => {
+      this.stopTransmission(task.identifier);
+      ImageStorage.instance.synchronize();
+    }
+    task.ontimeout = (task) => {
+      this.stopTransmission(task.identifier);
+      ImageStorage.instance.synchronize();
+    }
+  }
+
+  private startReceiveTransmission(identifier: string) {
     this.stopTransmission(identifier);
+    if (this.taskMap.has(identifier)) return;
 
-    this.transmissionTimers[identifier] = setTimeout(() => {
-      EventSystem.call('TIMEOUT_FILE_TRANSMISSION', { fileIdentifier: identifier }, Network.peerId);
-      this.stopTransmission(identifier);
-    }, 30 * 1000);
-
-    EventSystem.register(this.transmissionTimers[identifier])
-      .on('CLOSE_OTHER_PEER', 0, event => {
-        console.warn('送信キャンセル', event.data.peer);
-        EventSystem.call('TIMEOUT_FILE_TRANSMISSION', { fileIdentifier: identifier }, Network.peerId);
-        this.stopTransmission(identifier);
-      });
-    console.log('startFileTransmission => ', Object.keys(this.transmissionTimers).length);
+    let task = BufferSharingTask.createReceiveTask<ImageContext[]>(identifier);
+    this.taskMap.set(identifier, task);
+    task.onfinish = (task, data) => {
+      this.stopTransmission(task.identifier);
+      EventSystem.trigger('UPDATE_FILE_RESOURE', { identifier: task.identifier, updateImages: data });
+      ImageStorage.instance.synchronize();
+    }
+    task.ontimeout = (task) => {
+      this.stopTransmission(task.identifier);
+      ImageStorage.instance.synchronize();
+    }
+    console.log('startFileTransmission => ', this.taskMap.size);
   }
 
   private stopTransmission(identifier: string) {
-    if (!this.transmissionTimers[identifier]) return;
+    let task = this.taskMap.get(identifier);
+    if (task) { task.cancel(); }
+    this.taskMap.delete(identifier);
 
-    EventSystem.unregister(this.transmissionTimers[identifier]);
-    clearTimeout(this.transmissionTimers[identifier]);
-
-    this.transmissionTimers[identifier] = null;
-    delete this.transmissionTimers[identifier];
-
-    console.log('stopFileTransmission => ', Object.keys(this.transmissionTimers).length);
+    console.log('stopFileTransmission => ', this.taskMap.size);
   }
 
   private request(request: Catalog, peer: string) {
@@ -209,8 +169,54 @@ export class FileSharingSystem {
     EventSystem.call('REQUEST_FILE_RESOURE', { identifiers: request, receiver: Network.peerId, candidatePeers: peers }, peer);
   }
 
+  private makeSendUpdateImages(catalog: Catalog, maxSize: number = 1024 * 1024 * 0.5): ImageContext[] {
+    let updateImages: ImageContext[] = [];
+    let byteSize: number = 0;
+
+    // Fisher-Yates
+    for (let i = catalog.length - 1; 0 <= i; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      [catalog[i], catalog[rand]] = [catalog[rand], catalog[i]];
+    }
+
+    catalog.sort((a, b) => {
+      if (a.state < b.state) return -1;
+      if (a.state > b.state) return 1;
+      return 0;
+    });
+
+    for (let i = 0; i < catalog.length; i++) {
+      let item: { identifier: string, state: number } = catalog[i];
+      let image: ImageFile = ImageStorage.instance.get(item.identifier);
+
+      let context: ImageContext = { identifier: image.identifier, name: image.name, type: '', blob: null, url: null, thumbnail: { type: '', blob: null, url: null, } };
+
+      if (image.state === ImageState.URL) {
+        context.url = image.url;
+      } else if (item.state === ImageState.NULL) {
+        context.thumbnail.blob = image.thumbnail.blob;//
+        context.thumbnail.type = image.thumbnail.type;
+      } else {
+        context.blob = image.blob;//
+        context.type = image.blob.type;
+      }
+
+      let size = context.blob
+        ? context.blob.size
+        : context.thumbnail.blob
+          ? context.thumbnail.blob.size
+          : 100;
+
+      if (0 < byteSize && maxSize < byteSize + size) break;
+
+      updateImages.push(context);
+      byteSize += size;
+    }
+    return updateImages;
+  }
+
   private isTransmission(): boolean {
-    if (this.maxTransmission <= Object.keys(this.transmissionTimers).length) {
+    if (this.maxTransmission <= this.taskMap.size) {
       return true;
     } else {
       return false;
