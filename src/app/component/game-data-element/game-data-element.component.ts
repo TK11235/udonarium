dca159edd609bbec4c6087c38d0251b69548594f
TK11235 <TK11235@users.noreply.github.com>
2018-12-19 23:10:17 +0900

    fix: スライダーを操作した時のカクつきを改善

--- src/app/component/game-data-element/game-data-element.component.ts
+++ src/app/component/game-data-element/game-data-element.component.ts
@@ -94,13 +94,12 @@ export class GameDataElementComponent implements OnInit, OnDestroy, AfterViewIni
   }
 
   private setUpdateTimer() {
-    if (this.updateTimer === null && this.gameDataElement) {
-      this.updateTimer = setTimeout(() => {
-        if (this.gameDataElement.name !== this.name) this.gameDataElement.name = this.name;
-        if (this.gameDataElement.currentValue !== this.currentValue) this.gameDataElement.currentValue = this.currentValue;
-        if (this.gameDataElement.value !== this.value) this.gameDataElement.value = this.value;
-        this.updateTimer = null;
-      }, 66);
-    }
+    clearTimeout(this.updateTimer);
+    this.updateTimer = setTimeout(() => {
+      if (this.gameDataElement.name !== this.name) this.gameDataElement.name = this.name;
+      if (this.gameDataElement.currentValue !== this.currentValue) this.gameDataElement.currentValue = this.currentValue;
+      if (this.gameDataElement.value !== this.value) this.gameDataElement.value = this.value;
+      this.updateTimer = null;
+    }, 66);
   }
 }
