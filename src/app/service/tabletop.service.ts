66b9819d686d1eabeb9db2bc308dd349e3583176
TK11235 <TK11235@users.noreply.github.com>
2019-01-16 23:24:07 +0900

    refactor: テーブルの初期化処理をServiceに移管

--- src/app/service/tabletop.service.ts
+++ src/app/service/tabletop.service.ts
@@ -1,4 +1,9 @@
 import { Injectable, NgZone } from '@angular/core';
+import { ImageContext, ImageFile } from '@udonarium/core/file-storage/image-file';
+import { ImageStorage } from '@udonarium/core/file-storage/image-storage';
+import { GameCharacter } from '@udonarium/game-character';
+import { GameTable } from '@udonarium/game-table';
+import { TableSelecter } from '@udonarium/table-selecter';
 
 import { PointerDeviceService } from './pointer-device.service';
 
@@ -10,4 +15,84 @@ export class TabletopService {
     public ngZone: NgZone,
     public pointerDeviceService: PointerDeviceService,
   ) { }
+
+  makeDefaultTable() {
+    let tableSelecter = new TableSelecter('tableSelecter');
+    tableSelecter.initialize();
+
+    let gameTable = new GameTable('gameTable');
+    let testBgFile: ImageFile = null;
+    let bgFileContext = ImageFile.createEmpty('testTableBackgroundImage_image').toContext();
+    bgFileContext.url = './assets/images/BG10a_80.jpg';
+    testBgFile = ImageStorage.instance.add(bgFileContext);
+    let testDistanceFile: ImageFile = null;
+    let distanceFileContext = ImageFile.createEmpty('testTableDistanceviewImage_image').toContext();
+    distanceFileContext.url = './assets/images/BG00a1_80.jpg';
+    testDistanceFile = ImageStorage.instance.add(distanceFileContext);
+    gameTable.name = '最初のテーブル';
+    gameTable.imageIdentifier = testBgFile.identifier;
+    gameTable.distanceviewImageIdentifier = testDistanceFile.identifier;
+    gameTable.width = 20;
+    gameTable.height = 15;
+    gameTable.initialize();
+
+    tableSelecter.viewTableIdentifier = gameTable.identifier;
+  }
+
+  makeDefaultTabletopObjects() {
+    let testCharacter: GameCharacter = null;
+    let testFile: ImageFile = null;
+    let fileContext: ImageContext = null;
+
+    testCharacter = new GameCharacter('testCharacter_1');
+    fileContext = ImageFile.createEmpty('testCharacter_1_image').toContext();
+    fileContext.url = './assets/images/mon_052.gif';
+    testFile = ImageStorage.instance.add(fileContext);
+    testCharacter.location.x = 5 * 50;
+    testCharacter.location.y = 9 * 50;
+    testCharacter.initialize();
+    testCharacter.createTestGameDataElement('モンスターA', 1, testFile.identifier);
+
+    testCharacter = new GameCharacter('testCharacter_2');
+    testCharacter.location.x = 8 * 50;
+    testCharacter.location.y = 8 * 50;
+    testCharacter.initialize();
+    testCharacter.createTestGameDataElement('モンスターB', 1, testFile.identifier);
+
+    testCharacter = new GameCharacter('testCharacter_3');
+    fileContext = ImageFile.createEmpty('testCharacter_3_image').toContext();
+    fileContext.url = './assets/images/mon_128.gif';
+    testFile = ImageStorage.instance.add(fileContext);
+    testCharacter.location.x = 4 * 50;
+    testCharacter.location.y = 2 * 50;
+    testCharacter.initialize();
+    testCharacter.createTestGameDataElement('モンスターC', 3, testFile.identifier);
+
+    testCharacter = new GameCharacter('testCharacter_4');
+    fileContext = ImageFile.createEmpty('testCharacter_4_image').toContext();
+    fileContext.url = './assets/images/mon_150.gif';
+    testFile = ImageStorage.instance.add(fileContext);
+    testCharacter.location.x = 6 * 50;
+    testCharacter.location.y = 11 * 50;
+    testCharacter.initialize();
+    testCharacter.createTestGameDataElement('キャラクターA', 1, testFile.identifier);
+
+    testCharacter = new GameCharacter('testCharacter_5');
+    fileContext = ImageFile.createEmpty('testCharacter_5_image').toContext();
+    fileContext.url = './assets/images/mon_211.gif';
+    testFile = ImageStorage.instance.add(fileContext);
+    testCharacter.location.x = 12 * 50;
+    testCharacter.location.y = 12 * 50;
+    testCharacter.initialize();
+    testCharacter.createTestGameDataElement('キャラクターB', 1, testFile.identifier);
+
+    testCharacter = new GameCharacter('testCharacter_6');
+    fileContext = ImageFile.createEmpty('testCharacter_6_image').toContext();
+    fileContext.url = './assets/images/mon_135.gif';
+    testFile = ImageStorage.instance.add(fileContext);
+    testCharacter.initialize();
+    testCharacter.location.x = 5 * 50;
+    testCharacter.location.y = 13 * 50;
+    testCharacter.createTestGameDataElement('キャラクターC', 1, testFile.identifier);
+  }
 }
