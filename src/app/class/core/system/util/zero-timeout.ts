2005392185bb9c137f08f817a812bef65418b786
TK11235 <TK11235@users.noreply.github.com>
2019-03-27 01:00:16 +0900

    refactor: Mapを使用する

--- src/app/class/core/system/util/zero-timeout.ts
+++ src/app/class/core/system/util/zero-timeout.ts
@@ -1,24 +1,24 @@
 let i: number = 0;
-const timeouts = {};
+const timeouts = new Map<number, Function>();
 const channel = new MessageChannel();
 
 export function setZeroTimeout(fn): number {
   if (i === 0x100000000) // max queue size
     i = 0;
   if (++i in timeouts) throw new Error('setZeroTimeout queue overflow.');
-  timeouts[i] = fn;
+  timeouts.set(i, fn);
   channel.port2.postMessage(i);
   return i;
 }
 
 export function clearZeroTimeout(id: number) {
-  delete timeouts[id];
+  timeouts.delete(id);
 }
 
 channel.port1.onmessage = function (ev) {
-  let fn = timeouts[ev.data];
+  const fn = timeouts.get(ev.data);
   if (fn) {
-    delete timeouts[ev.data];
+    timeouts.delete(ev.data);
     fn();
   }
 }
