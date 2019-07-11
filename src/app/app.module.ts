800ca61978eb9bc20dd06a4b53b8792af10525c7
TK11235 <TK11235@users.noreply.github.com>
2019-07-11 22:47:02 +0900

    fix: 使用していないModuleを削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -103,8 +102,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [
     AppConfigService,
