e8807b83e79649ffdfe1bf21e181d07e6480a8c5
TK11235 <TK11235@users.noreply.github.com>
2019-08-05 22:09:12 +0900

    refactor: コードの短縮

--- src/app/class/core/system/util/xml-util.ts
+++ src/app/class/core/system/util/xml-util.ts
@@ -4,17 +4,15 @@ export namespace XmlUtil {
     let xmlDocument: Document = null;
     try {
       xmlDocument = domParser.parseFromString(xml, 'application/xml');
-      if (xmlDocument.getElementsByTagName('parsererror').length) {
+      let parsererror = xmlDocument.getElementsByTagName('parsererror');
+      if (parsererror.length) {
+        console.error('XMLのパースに失敗しました', xmlDocument.documentElement);
         xmlDocument = null;
       }
     } catch (error) {
       console.error(error);
     }
-    if (!xmlDocument) {
-      console.error('XMLのパースに失敗しました');
-      return null;
-    }
-    return xmlDocument.documentElement;
+    return xmlDocument ? xmlDocument.documentElement : null;
   }
 
   export function encodeEntityReference(string: string): string {
