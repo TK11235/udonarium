25f5fdbea24f7afc7d4e61bab7ea624e9796eb64
TK11235 <TK11235@users.noreply.github.com>
2019-10-12 01:10:15 +0900

    refactor: 座標移動量の算出タイミング

--- src/app/component/game-table/game-table.component.ts
+++ src/app/component/game-table/game-table.component.ts
@@ -72,9 +72,15 @@ export class GameTableComponent implements OnInit, OnDestroy, AfterViewInit {
   private input: InputHandler = null;
 
   private hammer: HammerManager = null;
+  private currentHammerDeltaX: number = 0;
   private currentHammerDeltaY = 1.0;
   private currentHammerScale = 1.0;
   private currentHammerRotation = 0;
+
+  private prevHammerDeltaX: number = 0;
+  private prevHammerDeltaY: number = 0;
+  private prevHammerScale: number = 0;
+  private prevHammerRotation: number = 0;
 
   get characters(): GameCharacter[] { return this.tabletopService.characters; }
   get tableMasks(): GameTableMask[] { return this.tabletopService.tableMasks; }
@@ -145,57 +151,74 @@ export class GameTableComponent implements OnInit, OnDestroy, AfterViewInit {
 
     this.hammer.add([pan, pinch, rotate]);
 
-    this.hammer.on('panstart', this.onPanStart.bind(this));
-    this.hammer.on('panmove', this.onPanMove.bind(this));
+    this.hammer.on('hammer.input', this.onHammer.bind(this));
+    this.hammer.on('pan1pstart', this.onTappedPanStart.bind(this));
+    this.hammer.on('pan1pmove', this.onTappedPanMove.bind(this));
+    this.hammer.on('pan1pend', this.onTappedPanEnd.bind(this));
+    this.hammer.on('pan1pcancel', this.onTappedPanEnd.bind(this));
+    this.hammer.on('pan2pstart', this.onPanStart.bind(this));
+    this.hammer.on('pan2pmove', this.onPanMove.bind(this));
     this.hammer.on('pinchstart', this.onPinchStart.bind(this));
     this.hammer.on('pinchmove', this.onPinchMove.bind(this));
     this.hammer.on('rotatestart', this.onRotateStart.bind(this));
     this.hammer.on('rotatemove', this.onRotateMove.bind(this));
   }
 
+  onHammer(ev: HammerInput) {
+    if (ev.isFirst) {
+      this.currentHammerScale = ev.scale;
+      this.currentHammerRotation = ev.rotation;
+      this.currentHammerDeltaX = ev.deltaX;
+      this.currentHammerDeltaY = ev.deltaY;
+    } else {
+      this.currentHammerScale = ev.scale - this.prevHammerScale;
+      this.currentHammerRotation = ev.rotation - this.prevHammerRotation;
+      this.currentHammerDeltaX = ev.deltaX - this.prevHammerDeltaX;
+      this.currentHammerDeltaY = ev.deltaY - this.prevHammerDeltaY;
+    }
+    this.prevHammerScale = ev.scale;
+    this.prevHammerRotation = ev.rotation;
+    this.prevHammerDeltaX = ev.deltaX;
+    this.prevHammerDeltaY = ev.deltaY;
+  }
+
   onPanStart(ev: HammerInput) {
     this.cancelInput();
-    this.currentHammerDeltaY = ev.deltaY;
   }
 
   onPanMove(ev: HammerInput) {
     this.cancelInput();
-    let rotateX = -(ev.deltaY - this.currentHammerDeltaY) / window.innerHeight * 100;
+    let rotateX = -this.currentHammerDeltaY / window.innerHeight * 100;
 
     if (80 < rotateX + this.viewRotateX) rotateX += 80 - (rotateX + this.viewRotateX);
     if (rotateX + this.viewRotateX < 0) rotateX += 0 - (rotateX + this.viewRotateX);
 
     this.setTransform(0, 0, 0, rotateX, 0, 0);
-    this.currentHammerDeltaY = ev.deltaY;
   }
 
   onPinchStart(ev: HammerInput) {
     this.cancelInput();
-    this.currentHammerScale = ev.scale;
   }
 
   onPinchMove(ev: HammerInput) {
     this.cancelInput();
-    let scale = ev.scale - this.currentHammerScale;
+    let scale = this.currentHammerScale;
     let transformZ = scale * 500;
 
     if (750 < transformZ + this.viewPotisonZ) transformZ += 750 - (transformZ + this.viewPotisonZ);
 
     this.setTransform(0, 0, transformZ, 0, 0, 0);
-    this.currentHammerScale = ev.scale;
   }
 
   onRotateStart(ev: HammerInput) {
     this.cancelInput();
-    this.currentHammerRotation = ev.rotation;
   }
 
   onRotateMove(ev: HammerInput) {
     this.cancelInput();
-    let rotation = ev.rotation - this.currentHammerRotation;
+    let rotation = this.currentHammerRotation;
     let rotateZ = rotation;
     this.setTransform(0, 0, 0, 0, 0, rotateZ);
-    this.currentHammerRotation = ev.rotation;
   }
 
   onInputStart(e: any) {
