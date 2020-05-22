582eaadd6119b63df9b966af3af29dd0154e7656
TK11235 <TK11235@users.noreply.github.com>
2020-05-23 00:02:44 +0900

    fix: グリッドの表示状態を適切に更新する

--- src/app/component/game-table-setting/game-table-setting.component.ts
+++ src/app/component/game-table-setting/game-table-setting.component.ts
@@ -48,6 +48,7 @@ export class GameTableSettingComponent implements OnInit, OnDestroy, AfterViewIn
   get tableGridShow(): boolean { return this.tableSelecter.gridShow; }
   set tableGridShow(tableGridShow: boolean) {
     this.tableSelecter.gridShow = tableGridShow;
+    EventSystem.trigger('UPDATE_GAME_OBJECT', this.tableSelecter.toContext()); // 自分にだけイベントを発行してグリッド更新を誘発
   }
 
   get tableGridSnap(): boolean { return this.tableSelecter.gridSnap; }
