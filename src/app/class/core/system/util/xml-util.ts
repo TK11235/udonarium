ce4d971c653c19000b43417b422e7c03c97e0fab
TK11235 <TK11235@users.noreply.github.com>
2020-10-13 00:48:11 +0900

    perf: エンティティ参照の置換処理を改善

--- src/app/class/core/system/util/xml-util.ts
+++ src/app/class/core/system/util/xml-util.ts
@@ -1,4 +1,22 @@
 export namespace XmlUtil {
+  const encodePattern = /&|<|>|"|'/g;
+  const encodeMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '\"': '&quot;',
+    '\'': '&apos;',
+  };
+
+  const decodePattern = /&amp;|&lt;|&gt;|&quot;|&apos;/g;
+  const decodeMap = {
+    '&amp;': '&',
+    '&lt;': '<',
+    '&gt;': '>',
+    '&quot;': '\"',
+    '&apos;': '\'',
+  };
+
   export function xml2element(xml: string) {
     let domParser: DOMParser = new DOMParser();
     let xmlDocument: Document = null;
@@ -17,14 +35,22 @@ export namespace XmlUtil {
   }
 
   export function encodeEntityReference(string: string): string {
-    return string.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return string.replace(encodePattern, encodeReplacer);
   }
 
   export function decodeEntityReference(string: string): string {
-    return string.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '\"').replace(/&amp;/g, '&');
+    return string.replace(decodePattern, decodeReplacer);
   }
 
   function sanitizeXml(xml: string): string {
     return xml.replace(/([^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFC\u{10000}-\u{10FFFF}])/ug, '').trim();
+  }
+
+  function encodeReplacer(char: string): string {
+    return encodeMap[char];
+  }
+
+  function decodeReplacer(entity: string): string {
+    return decodeMap[entity];
   }
 }
