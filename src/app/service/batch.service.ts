087aca94223ab1f25aec9786ec62c0ade336f0f6
TK11235 <TK11235@users.noreply.github.com>
2022-02-14 22:35:27 +0900

    fix: バッチ処理の動作改善
    最初に予約されたバッチ処理を孤立させず、さらにバッチ全体をZoneの外で実行することを保証する.

--- src/app/service/batch.service.ts
+++ src/app/service/batch.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { setZeroTimeout } from '@udonarium/core/system/util/zero-timeout';
 
 type BatchTask = () => void;
 
@@ -9,12 +10,18 @@ export class BatchService {
   private batchTask: Map<any, BatchTask> = new Map();
   private batchTaskTimer: NodeJS.Timer = null;
 
-  constructor() { }
+  constructor(
+    private ngZone: NgZone,
+  ) { }
 
   add(task: BatchTask, key: any = {}) {
     this.batchTask.set(key, task);
     if (this.batchTaskTimer != null) return;
-    this.execBatch();
+    setZeroTimeout(() => this.execBatch());
+    this.ngZone.runOutsideAngular(() => this.startTimer());
+  }
+
+  private startTimer() {
     this.batchTaskTimer = setInterval(() => {
       if (0 < this.batchTask.size) {
         this.execBatch();
