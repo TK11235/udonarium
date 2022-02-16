9479915540f938e414d03cdf1a9bb735b0b70ebd
TK11235 <TK11235@users.noreply.github.com>
2022-02-17 03:05:45 +0900

    perf: ChangeDetectionが要求される回数を最適化

--- src/app/component/chat-input/chat-input.component.ts
+++ src/app/component/chat-input/chat-input.component.ts
@@ -133,7 +133,7 @@ export class ChatInputComponent implements OnInit, OnDestroy {
         }
         this.writingPeers.get(event.sendFrom).reset();
         this.updateWritingPeerNames();
-        this.batchService.add(() => this.ngZone.run(() => { }), this);
+        this.batchService.requireChangeDetection();
       });
   }
 
