3cabf0a71e03f76db310a6bb5998b61e46a54335
TK11235 <TK11235@users.noreply.github.com>
2022-02-19 01:49:48 +0900

    feat: Databaseクラスを汎用的なKey-Valueストアとして利用できるように改修

--- src/app/class/database/database.ts
+++ src/app/class/database/database.ts
@@ -1,173 +1,160 @@
-import { PromiseQueue } from '../core/system/util/promise-queue';
-
-interface PeerHistory {
-  peerId: string,
-  timestamp: number,
-  history: string[]
-}
-
 // 試験実装中
-export class Database {
-  private static DB_NAME: string = 'UdonariumDataBase';
-  private static VERSION: number = 1;
+export class Database<T> {
+  readonly version: number = 1;
+  readonly databaseName: string;
+  readonly storeName: string;
 
-  private queue: PromiseQueue = new PromiseQueue('DatabaseQueue');
-  private db: IDBDatabase;
+  private openDBPromise: Promise<IDBDatabase>;
 
-  constructor() {
-    this.openDB(Database.DB_NAME, Database.VERSION);
+  constructor(name: string) {
+    this.databaseName = `Udonarium-IDB-${name}`;
+    this.storeName = 'ObjectStore';
+    this.open();
   }
 
-  private openDB(dbName: string, version: number): Promise<IDBDatabase> {
-    return this.queue.add((resolve, reject) => {
-      console.log('openDB');
-      let request = indexedDB.open(dbName, version);
-      request.onerror = (event) => {
-        console.error(request.error, event);
-        if (request.error.name === 'VersionError') {
-          indexedDB.deleteDatabase(dbName);
-          this.openDB(dbName, version);
-        }
-        // request.errorCode に対して行うこと!
-        resolve();
-      };
-      request.onblocked = (event) => {
-        console.warn('openDB onblocked');
+  async open(): Promise<IDBDatabase> {
+    if (this.openDBPromise) return this.openDBPromise;
+
+    let openFunc = async () => {
+      let request = indexedDB.open(this.databaseName, this.version);
+      request.onblocked = event => {
+        console.warn('request.onblocked');
         // 他のタブがデータベースを読み込んでいる場合は、処理を進める前に
         // それらを閉じなければなりません。
-        alert('このサイトを開いている他のタブをすべて閉じてください!');
+        //alert('このサイトを開いている他のタブをすべて閉じてください!');
       };
-      request.onupgradeneeded = (event) => {
-        console.log('openDB onupgradeneeded');
-        this.initializeDB(request.result);
-        this.createStores();
+      request.onupgradeneeded = event => {
+        console.log('request.onupgradeneeded');
+        this.createObjectStore(request.result);
       };
-      request.onsuccess = (event) => {
-        console.log('openDB onsuccess');
-        this.initializeDB(request.result);
-        resolve();
-      };
-    });
-  }
 
-  private createStores() {
-    console.log('createStores');
-    if (this.db.objectStoreNames.contains('PeerHistory')) {
-      this.db.deleteObjectStore('PeerHistory');
-    }
-    let objectStore = this.db.createObjectStore('PeerHistory', { keyPath: 'peerId' });
-    objectStore.createIndex('timestamp', 'timestamp', { unique: false });
-    // データを追加する前に objectStore の作成を完了させるため、 
-    // transaction oncomplete を使用します。
-    objectStore.transaction.oncomplete = (event) => {
-      console.log('createStores oncomplete');
-      // 新たに作成した objectStore に値を保存します。
-      /*
-      var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-      for (var i in customerData) {
-        customerObjectStore.add(customerData[i]);
+      try {
+        let database = await this.waitFor(request);
+        return this.initializeDB(database);
+      } catch (e) {
+        console.error(e);
+        if (request.error.name === 'VersionError') {
+          console.log(`recreate <${this.databaseName}>`);
+          try {
+            await this.waitFor(indexedDB.deleteDatabase(this.databaseName));
+          } catch (e) {
+            console.warn(e);
+          }
+          return await openFunc();
+        }
+        throw e;
       }
-      */
     };
+    this.openDBPromise = openFunc();
+    return this.openDBPromise;
   }
 
-  private initializeDB(db: IDBDatabase) {
-    console.log('initializeDB');
+  async close() {
+    if (!this.openDBPromise) return;
+    try {
+      let database = await this.open();
+      database.close();
+      this.openDBPromise = null;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  private async createObjectStore(database: IDBDatabase) {
+    if (database.objectStoreNames.contains(this.storeName)) {
+      database.deleteObjectStore(this.storeName);
+    }
+    database.createObjectStore(this.storeName);
+  }
+
+  private initializeDB(database: IDBDatabase): IDBDatabase {
     // 別のページがバージョン変更を求めた場合に、通知されるようにするためのハンドラを追加するようにしてください。
     // データベースを閉じなければなりません。データベースを閉じると、別のページがデータベースをアップグレードできます。
     // これを行わなければ、ユーザがタブを閉じるまでデータベースはアップグレードされません。
-    db.onversionchange = (event) => {
-      console.warn('db.onversionchange.');
-      db.close();
-      alert('新しいバージョンのページが使用可能になりました。再読み込みしてください!');
+    database.onversionchange = event => {
+      console.warn('database.onversionchange.');
+      database.close();
+      this.openDBPromise = null;
+      //alert('新しいバージョンのページが使用可能になりました。再読み込みしてください!');
     };
-    db.onabort = (event) => {
-      console.error(event);
-    };
-    db.onerror = (event) => {
-      console.error(event);
-    };
-    this.db = db;
-    // データベースを使用する処理
+    database.onabort = database.onerror = event => console.error(event);
+    return database;
   }
 
-  addPeerHistory(myPeerId: string, otherPeerIds: string[]) {
-    this.queue.add((resolve, reject) => {
-      console.log('addPeerHistory');
-      let transaction = this.db.transaction(['PeerHistory'], 'readwrite');
-      let store = transaction.objectStore('PeerHistory');
-
-      transaction.oncomplete = (event) => {
-        console.log('addPeerHistory done.', 'readwrite');
-        resolve();
-      };
-
-      transaction.onerror = (event) => {
-        console.error(event);
-        resolve();
-      };
-
-      let history: PeerHistory = {
-        peerId: myPeerId,
-        timestamp: Date.now(),
-        history: otherPeerIds
-      }
-
-      store.put(history);
-    });
+  async getObjectStore(mode: IDBTransactionMode = 'readonly'): Promise<IDBObjectStore> {
+    let database = await this.open();
+    let transaction = database.transaction(this.storeName, mode);
+    return transaction.objectStore(this.storeName);
   }
 
-  deletePeerHistory(peerId: string) {
-    this.queue.add((resolve, reject) => {
-      let transaction = this.db.transaction(['PeerHistory'], 'readwrite');
-      let store = transaction.objectStore('PeerHistory');
-      transaction.oncomplete = (event) => {
-        console.log('addPeerHistory done.', 'readwrite');
-        resolve();
-      };
-
-      transaction.onerror = (event) => {
-        console.error(event);
-        resolve();
-      };
-
-      store.delete(peerId);
-    });
+  async get(key: IDBValidKey): Promise<T> {
+    try {
+      let store = await this.getObjectStore('readonly');
+      let request = store.get(key);
+      return await this.waitFor<T>(request);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
   }
 
-  getPeerHistory(): Promise<PeerHistory[]> {
-    return this.queue.add((resolve, reject) => {
-      console.log('getPeerHistory');
-      let transaction = this.db.transaction(['PeerHistory'], 'readonly');
-      let store = transaction.objectStore('PeerHistory');
-      let request = store.openCursor();
-      let history: PeerHistory[] = [];
+  async put(key: IDBValidKey, value: T): Promise<IDBValidKey> {
+    try {
+      let store = await this.getObjectStore('readwrite');
+      let request = store.put(value, key);
+      return await this.waitFor(request);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
 
-      transaction.oncomplete = (event) => {
-        console.log('getPeerHistory done.');
-        resolve(history);
-      };
+  async delete(key: IDBValidKey): Promise<void> {
+    try {
+      let store = await this.getObjectStore('readwrite');
+      let request = store.delete(key);
+      return await this.waitFor(request);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
 
-      transaction.onerror = (event) => {
-        console.error(event);
-        resolve(history);
-      };
+  async getAll(): Promise<T[]> {
+    try {
+      let store = await this.getObjectStore('readonly');
+      let request = store.getAll();
+      return await this.waitFor<T[]>(request);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
 
-      request.onerror = (event) => {
-        console.error(event);
-        resolve(history);
-      };
+  async getAllKeys(): Promise<IDBValidKey[]> {
+    try {
+      let store = await this.getObjectStore('readonly');
+      let request = store.getAllKeys();
+      return await this.waitFor(request);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
 
-      request.onsuccess = (event) => {
-        let cursor: IDBCursorWithValue = request.result;
-        if (cursor) {
-          console.log('id:' + cursor.key + ' value:', cursor.value);
-          history.push(cursor.value);
-          cursor.continue();
-        } else {
-          console.log('Entries all displayed.');
-        }
-      };
-    });
+  private waitFor(transaction: IDBTransaction): Promise<void>
+  private waitFor<T>(request: IDBRequest<T>): Promise<T>
+  private waitFor(arg: IDBTransaction | IDBRequest<any>): Promise<any> {
+    if (arg instanceof IDBTransaction) {
+      return new Promise((resolve, reject) => {
+        arg.oncomplete = event => resolve(null);
+        arg.onerror = arg.onabort = event => reject(arg.error);
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        arg.onsuccess = event => resolve(arg.result);
+        arg.onerror = event => reject(arg.error);
+      });
+    }
   }
 }
