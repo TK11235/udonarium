9982a768cf6a912b93dbb2c87065f6a20a48f62e
TK11235 <TK11235@users.noreply.github.com>
2022-10-30 21:29:39 +0900

    feat: bcdice更新
    i18nのDynamic Import対応

--- src/app/class/bcdice/bcdice-loader.ts
+++ src/app/class/bcdice/bcdice-loader.ts
@@ -1,7 +1,15 @@
-import Loader from 'bcdice/lib/loader/loader';
+import Loader, { I18nJsonObject } from 'bcdice/lib/loader/loader';
 
 // bcdice-js custom loader class
 export default class BCDiceLoader extends Loader {
+  async dynamicImportI18n(baseClassName: string, locale: string): Promise<I18nJsonObject> {
+    return (await import(
+      /* webpackChunkName: "lib/bcdice/i18n/[request]" */
+      /* webpackInclude: /\.json$/ */
+      /* webpackExclude: /i18n.json$/ */
+      `bcdice/lib/bcdice/i18n/${baseClassName}.${locale}.json`)).default as I18nJsonObject;
+  }
+
   async dynamicImport(className: string): Promise<void> {
     await import(
       /* webpackChunkName: "lib/bcdice/game_system/[request]" */
