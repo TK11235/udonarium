430cd013b3adeb554bdbcc8c9844d4c6e538dc25
TK11235 <TK11235@users.noreply.github.com>
2023-04-13 23:54:47 +0900

    feat: 効果音のローカル再生

--- src/app/class/sound-effect.ts
+++ src/app/class/sound-effect.ts
@@ -72,4 +72,20 @@ export class SoundEffect extends GameObject {
   private static _play(identifier: string) {
     EventSystem.call('SOUND_EFFECT', identifier);
   }
+
+  static playLocal(identifier: string)
+  static playLocal(audio: AudioFile)
+  static playLocal(arg: any) {
+    let identifier = '';
+    if (typeof arg === 'string') {
+      identifier = arg;
+    } else {
+      identifier = arg.identifier;
+    }
+    SoundEffect._playLocal(identifier);
+  }
+
+  private static _playLocal(identifier: string) {
+    EventSystem.trigger('SOUND_EFFECT', identifier);
+  }
 }
