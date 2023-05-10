7c107976b70bca35c2b6af5c7e99c66a7f9fe015
TK11235 <TK11235@users.noreply.github.com>
2023-05-10 23:32:41 +0900

    refactor: 引数の型定義

--- src/app/class/core/system/util/string-util.ts
+++ src/app/class/core/system/util/string-util.ts
@@ -1,5 +1,5 @@
 export namespace StringUtil {
-  export function toHalfWidth(str: String): string {
+  export function toHalfWidth(str: string): string {
     return str.replace(/[！-～]/g, c => String.fromCharCode(c.charCodeAt(0) - 0xFEE0));
   }
 }
