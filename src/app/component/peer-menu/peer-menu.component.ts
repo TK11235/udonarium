e0a841a2ac03da551c772d4268166236d3e813cd
TK11235 <TK11235@users.noreply.github.com>
2024-04-21 20:25:16 +0900

    feat: 新しいSkyWaではプライベート接続ボタンを非表示にする
    新しいSkyWayのChannelの概念の都合からユドナリウムのプライベート接続をオミットしている

--- src/app/component/peer-menu/peer-menu.component.ts
+++ src/app/component/peer-menu/peer-menu.component.ts
@@ -8,7 +8,7 @@ import { PeerCursor } from '@udonarium/peer-cursor';
 
 import { FileSelecterComponent } from 'component/file-selecter/file-selecter.component';
 import { LobbyComponent } from 'component/lobby/lobby.component';
-import { AppConfigService } from 'service/app-config.service';
+import { AppConfig, AppConfigService } from 'service/app-config.service';
 import { ModalService } from 'service/modal.service';
 import { PanelService } from 'service/panel.service';
 
@@ -27,6 +27,9 @@ export class PeerMenuComponent implements OnInit, OnDestroy, AfterViewInit {
 
   private interval: NodeJS.Timeout;
   get myPeer(): PeerCursor { return PeerCursor.myCursor; }
+
+  get config(): AppConfig { return AppConfigService.appConfig; }
+  get canUsePrivateSession(): boolean { return this.config.backend.mode != 'skyway2023'; }
 
   constructor(
     private ngZone: NgZone,
