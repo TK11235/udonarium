a3b1d63814ff7de961ae18f0cbc15dc22396021d
TK11235 <TK11235@users.noreply.github.com>
2024-09-05 00:09:23 +0900

    feat: 圧縮処理をpakoからCompression Streams APIに移行

--- src/app/class/core/system/util/compress.ts
+++ src/app/class/core/system/util/compress.ts
@@ -1,45 +1,12 @@
-import Pako from 'pako';
-
-import { setZeroTimeout } from './zero-timeout';
-
-export async function compressAsync(data: Uint8Array, chunkSize?: number): Promise<Uint8Array> {
-  const deflate = new Pako.Deflate({ level: 2, gzip: true });
-
-  try {
-    await processAsync(deflate, data, chunkSize);
-    return deflate.result as Uint8Array;
-  } catch (e) {
-    console.error(e);
-  }
-  return null;
+export async function compressAsync(data: Uint8Array): Promise<Uint8Array> {
+  return processAsync(new CompressionStream('gzip'), data);
 }
 
-export async function decompressAsync(data: Uint8Array, chunkSize?: number): Promise<Uint8Array> {
-  const inflate = new Pako.Inflate();
-
-  try {
-    await processAsync(inflate, data, chunkSize);
-    return inflate.result as Uint8Array;
-  } catch (e) {
-    console.error(e);
-  }
-  return null;
+export async function decompressAsync(data: Uint8Array): Promise<Uint8Array> {
+  return processAsync(new DecompressionStream('gzip'), data);
 }
 
-async function processAsync<T extends Pako.Deflate | Pako.Inflate>(pakoObj: T, data: Uint8Array, chunkSize: number = 16 * 1024): Promise<T> {
-  let total = Math.ceil(data.byteLength / chunkSize);
-  let sliceData: Uint8Array = null;
-
-  for (let sliceIndex = 0; sliceIndex < total; sliceIndex++) {
-    await waitTickAsync();
-    sliceData = data.slice(sliceIndex * chunkSize, (sliceIndex + 1) * chunkSize);
-    pakoObj.push(sliceData, total <= sliceIndex + 1);
-  }
-
-  if (pakoObj.err) throw new Error(`error: ${pakoObj.err}`);
-  return pakoObj;
-}
-
-function waitTickAsync(): Promise<void> {
-  return new Promise(resolve => setZeroTimeout(resolve));
+async function processAsync(transform: ReadableWritablePair, data: Uint8Array): Promise<Uint8Array> {
+  const stream = new Blob([data]).stream().pipeThrough(transform);
+  return new Uint8Array(await new Response(stream).arrayBuffer());
 }
